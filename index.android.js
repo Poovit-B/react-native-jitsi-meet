cba837706ce44594423e50a9b1c60382152c01fd
Poovit-B <61401085+Poovit-B@users.noreply.github.com>
2021-11-09 15:55:20 +0700

    update IOS featureFlag

--- index.android.js
+++ index.android.js
@@ -8,12 +8,14 @@ export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
 export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
 const call = JitsiMeetModule.call;
 const audioCall = JitsiMeetModule.audioCall;
-JitsiMeetModule.call = (url, userInfo) => {
+JitsiMeetModule.call = (url, userInfo, featureFlags) => {
   userInfo = userInfo || {};
+  featureFlags = featureFlags || {};
   call(url, userInfo);
 }
-JitsiMeetModule.audioCall = (url, userInfo) => {
+JitsiMeetModule.audioCall = (url, userInfo, featureFlags) => {
   userInfo = userInfo || {};
+  featureFlags = featureFlags || {};
   audioCall(url, userInfo);
 }
 export default JitsiMeetModule;
